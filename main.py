4ee559a0a3fdcdcb18aa7fb4114fecc0c62873ea
venikalaxmisaravanan <venikalaxmi06@gmail.com>
2026-08-01 17:21:57 +0530

    setup for llm chat interface

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-from tools import add_task, list_tasks, complete_task, delete_task
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import json
+
+from tools import (
+    add_task,
+    list_tasks,
+    complete_task,
+    delete_task,
+)
+
+from tool_schema import tools
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+system_prompt = """
+You are an AI Todo Assistant.
+
+You help users manage their todo list.
+
+Whenever appropriate,
+use the available tools.
+
+Never invent task data.
+
+Always rely on the tools.
+"""
+messages = [
+    {
+        "role": "system",
+        "content": system_prompt
+    }
+]
 
 while True:
-    print("\n===== AI Todo Agent =====")
-    print("1. Add Task")
-    print("2. List Tasks")
-    print("3. Complete Task")
-    print("4. Delete Task")
-    print("5. Exit")
 
-    choice = input("\nEnter your choice (1-5): ")
+    user = input("\nYou: ")
 
-    if choice == "1":
-        task = input("Enter task: ")
-        print(add_task(task))
-
-    elif choice == "2":
-        print("\nTasks:")
-        print(list_tasks())
-
-    elif choice == "3":
-        task = input("Enter completed task name: ")
-        print(complete_task(task))
-
-    elif choice == "4":
-        task = input("Enter task to delete: ")
-        print(delete_task(task))
-
-    elif choice == "5":
-        print("Goodbye!")
+    if user.lower() == "exit":
         break
 
-    else:
-        print("Invalid choice. Try again.")
+    messages.append({
+        "role": "user",
+        "content": user
+    })
